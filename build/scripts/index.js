63562150f5113b225951f1960b0ff014873bafb9
Santiago Aguilar <130328638+Santiag0Aguilar@users.noreply.github.com>
2026-01-09 12:43:31 -0600

    Add files via upload

--- build/scripts/index.js
+++ build/scripts/index.js
@@ -3,12 +3,11 @@ import { actualizarContadorCarrito } from "./modules/ui/cartCounter.js";
 import { renderCarrito } from "./modules/ui/renderCar.js";
 import { initWhatsAppButton } from "./modules/utils/whatsappButton.js";
 import { initCartNotes, initNotes } from "./modules/ui/initNotes.js";
-import initHeader from "./modules/ui/initHeader.js";
 import renderColonias from "./modules/ui/renderColonias.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   await initMenuProductos();
-  initHeader();
+  /* initHeader(); */
   HeaderActive();
   getMenu();
   actualizarContadorCarrito();
